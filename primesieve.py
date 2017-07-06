c042e266f54ac9fe819379cd7a8404c2a91f24af
Arthur Thiele <Arthur@Arthurs-MacBook-Pro.local>
2017-07-06 16:10:37 +1000

    added prime sieve utility

---
+++ primesieve.py
@@ -0,0 +1,19 @@
+#this will be a good prime sieve that can be used to get primes up to a value n
+
+import numpy as np
+
+n = 2000000
+potentialprimes = range (0, n)
+isprime = [1]*n
+for p in potentialprimes[2:]:
+    if isprime[p] == 1:
+        for i in range (2, int(np.floor(n/p))+1):
+            if p*i < len(isprime):
+                isprime[p*i] = 0
+
+
+primes = []
+for i in range(2, n):
+    if isprime[i] == 1:
+        primes.append(potentialprimes[i])
+print primes[0], primes[1]
